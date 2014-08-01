0fb59a5eda830d591e070e385b538c15cd4639e5
Braden Walters <vc@braden-walters.info>
2014-07-31 23:54:55 -0400

    Export as CSV

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject itrace-gaze-analysis "1.0"
   :description "Analyses iTrace gaze data."
   :main itrace-gaze-analysis.core
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/data.csv "0.1.2"]])
--- src/itrace_gaze_analysis/core.clj
+++ src/itrace_gaze_analysis/core.clj
@@ -1,14 +1,28 @@
 (ns itrace-gaze-analysis.core (:gen-class))
 
-(require '[clojure.xml :as xml]
-         '[clojure.zip :as zip])
+(require '[clojure.java.io :as io]
+         '[clojure.xml :as xml]
+         '[clojure.zip :as zip]
+         '[clojure.data.csv :as csv])
 
 (defn read-xml-file
   "Reads an XML document specified by the first parameter and represents it as
    arrays and hashes."
-  [s]
-  (with-open [file (java.io.FileInputStream. (java.io.File. s))]
+  [file-path]
+  (with-open [file (java.io.FileInputStream. (java.io.File. file-path))]
     (zip/xml-zip (xml/parse file))))
+
+(defn write-csv-file
+  "Writes the hashmaps in parameter collection-to-write into the file specified
+   in the parameter file-path as a CSV file. The parameter key-order is a
+   collection of hashmap keys, which is used to determine the order in which
+   columns are exported."
+  [file-path collection-to-write key-order]
+  (with-open [file (io/writer file-path)]
+    (let [content (concat [key-order] ;Export keys.
+                          (map (fn [item] (map #(get item %) key-order))
+                               collection-to-write))]
+      (csv/write-csv file content))))
 
 (defn get-gazes-from-root
   "When the return value of read-xml-file is provided, an array of the gaze
@@ -97,18 +111,20 @@
                 (get-fully-qualified-names (only-method-declarations (flatten
                 (sources-each-gaze (get-gazes-from-root (read-xml-file
                 (nth args 1))))))))))]
-        (doseq [item res] (println item))))
+        (write-csv-file (nth args 2) res [:fullyQualifiedName :count])))
     "ranked-lines" (dorun
       (let [res (sort-distinct-by-count-desc (distinct-count (file+sces+lines
                 (get-gazes-from-root (read-xml-file (nth args 1))))))]
-        (doseq [item res] (println item))))
+        (write-csv-file (nth args 2) res
+                        [:line :file :fullyQualifiedNames :count])))
     "ranked-lines-in-method" (dorun
       (let [res (sort-distinct-by-count-desc (distinct-count (file+sces+lines
                 (only-method-named (get-gazes-from-root (read-xml-file
-                (nth args 1))) (nth args 2)))))]
-        (doseq [item res] (println item))))
+                (nth args 1))) (nth args 3)))))]
+        (write-csv-file (nth args 2) res
+                        [:line :file :fullyQualifiedNames :count])))
     (println (str "How would you like to run this program? Specify as args.\n"
-                  "  <prog.jar> ranked-method-decl <GAZE_FILE>\n"
-                  "  <prog.jar> ranked-lines <GAZE_FILE>\n"
-                  "  <prog.jar> ranked-lines-in-method <GAZE_FILE> "
+                  "  <prog.jar> ranked-method-decl <GAZE_FILE> <OUT_CSV>\n"
+                  "  <prog.jar> ranked-lines <GAZE_FILE> <OUT_CSV>\n"
+                  "  <prog.jar> ranked-lines-in-method <GAZE_FILE> <OUT_CSV> "
                   "<FULLY_QUALIFIED_METHOD_NAME>"))))
